1d87b0838e2c4e896be655624c0336bb1a76c8c9
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-18 12:00:05 +0530

    Added: Program to create Simple linked list 0f 56,30,70

---
+++ datastructures/MyNodeTest.java
@@ -0,0 +1,16 @@
+package datastructures;
+
+public class MyNodeTest {
+	public static void main(String[] args) {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		if (result == true)
+			System.out.println("LinkedList is created");
+		else
+			System.out.println("LinkedList not created");
+	}
+}
--- datastructures/Welcome.java
+++
@@ -1,7 +0,0 @@
-package datastructures;
-
-public class Welcome {
-	public static void main(String[] args) {
-		System.out.println("Welcome to LinkedList Programs");
-	}
-}
