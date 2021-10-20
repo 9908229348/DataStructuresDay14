b640065f49c7ef3f63c7a31451459567bbec9d80
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-20 11:15:47 +0530

    Added: Program to create queue using linkedlist

---
+++ queueusinglinkedlist/MyQueue.java
@@ -0,0 +1,27 @@
+package queueusinglinkedlist;
+
+import datastructures.MyLinkedList;
+import datastructures.INode;
+import datastructures.MyNode;
+
+public class MyQueue<K> {
+	private final MyLinkedList myList;
+
+	public MyQueue() {
+		this.myList = new MyLinkedList();
+	}
+
+	public void printStack() {
+		myList.printMyNodes();
+	}
+
+	public INode peak() {
+		return myList.head;
+	}
+
+	public void push(INode<K> myNode) {
+		myList.append(myNode);
+	}
+
+}
+
---
+++ queueusinglinkedlist/MyQueueTest.java
@@ -0,0 +1,25 @@
+package queueusinglinkedlist;
+
+import datastructures.INode;
+import datastructures.MyNode;
+
+public class MyQueueTest {
+	public static void main(String[] args) {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyQueue myQueue = new MyQueue();
+		myQueue.push(myFirstNode);
+		myQueue.push(mySecondNode);
+		myQueue.push(myThirdNode);
+
+		myQueue.printStack();
+		INode peak = myQueue.peak();
+		if (peak == myFirstNode) {
+			System.out.println("Nodes are Linked");
+		} else {
+			System.out.println("Not Linked");
+		}
+	}
+
+}
